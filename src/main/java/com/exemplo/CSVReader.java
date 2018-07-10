a12c2562a14c8e2e67d2f6ad52bf5ded9576e9ab
TaniaBogalho <tania.bogalho@criticalsoftware.com>
2018-07-10 08:41:23 +0100

    Update CSVReader class.

--- src/main/java/com/exemplo/CSVReader.java
+++ src/main/java/com/exemplo/CSVReader.java
@@ -2,7 +2,6 @@ package com.exemplo;
 
 import org.codehaus.jettison.json.JSONException;
 import org.codehaus.jettison.json.JSONObject;
-
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -12,16 +11,11 @@ public class CSVReader {
 
     //Delimiter used in CSV file
     private static final String COMMA_DELIMITER = ",";
-
-
-    public JSONObject readCsvFile(String fileName) {
+    JSONObject readCsvFile(String fileName) {
 
         BufferedReader fileReader = null;
-
         JSONObject json_obj_file = new JSONObject();
-
-        String line = "";
-
+        String line;
 
         try
         {
@@ -38,17 +32,9 @@ public class CSVReader {
                 String[] tokens = line.split(COMMA_DELIMITER);
 
                 if (tokens.length > 0) {
-
-                    /*Dados dados = new Dados();
-                    dados.setOp(tokens[0]);
-                    dados.setValue1(Double.parseDouble(tokens[1]));
-                    dados.setValue2(Double.parseDouble(tokens[2]));
-                    */
-
                     json_obj_file.put("op", tokens[0]);
                     json_obj_file.put("value1", tokens[1]);
                     json_obj_file.put("value2", tokens[2]);
-
                 }
             }
         } catch (FileNotFoundException e) {
@@ -65,6 +51,7 @@ public class CSVReader {
         finally
         {
             try {
+                assert fileReader != null;
                 fileReader.close();
             } catch (IOException e) {
                 System.out.println("Error while closing the fileReader!!!");
@@ -72,8 +59,6 @@ public class CSVReader {
                 e.printStackTrace();
             }
         }
-
-
         return json_obj_file;
     }
 }
