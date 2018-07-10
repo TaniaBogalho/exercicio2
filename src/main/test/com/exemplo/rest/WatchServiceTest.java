eac35de92cb935c9006d1fa58c60e85f5dafafaf
TaniaBogalho <tania.bogalho@criticalsoftware.com>
2018-07-10 08:44:39 +0100

    Update WatchServiceTest class.

--- src/main/test/com/exemplo/rest/WatchServiceTest.java
+++ src/main/test/com/exemplo/rest/WatchServiceTest.java
@@ -3,53 +3,22 @@ package com.exemplo.rest;
 import com.exemplo.WatchService;
 import org.apache.commons.io.FileUtils;
 import org.junit.jupiter.api.Test;
-
 import java.io.File;
 import java.io.IOException;
 
 
-//@RunWith(EasyMockRunner.class)
 class WatchServiceTest {
 
-    //Create de mock
-    /*@Mock
-    private Files file = EasyMock.createMock(Files.class);
-    */
-
-    //Set the tested class
-    /*@TestSubject
-    private WatchService watchService = new WatchService();*/
-
-    
-    
-    /*private static void createFile()
-    {
-        String sourcePath = "/home/tania/test_file.csv";
-        File source = new File(sourcePath);
-
-        String destPath = "/home/tania/input/test_file.csv";
-        File dest = new File(destPath);
-
-
-        try {
-            FileUtils.copyFile(source,dest);
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-    }*/
 
     @Test
-    void testReadCSVFile()
-    {
+    void testReadCSVFile() {
 
         WatchService watchService = new WatchService();
 
-        //Thread watchServicethread = new Thread(() -> watchService.readCSVFile());
-
         Thread watchServicethread = new Thread(watchService::readCSVFile);
+
         watchServicethread.start();
+
 
         try {
 
@@ -60,19 +29,17 @@ class WatchServiceTest {
         }
 
 
-        //Thread createFilethread = new Thread(WatchServiceTest::createFile);
-
         Thread createFilethread = new Thread(() ->
         {
-            String sourcePath = "/home/tania/test_file.csv";
+            String sourcePath = "/home/tania/test_file2.csv";
             File source = new File(sourcePath);
 
-            String destPath = "/home/tania/input/test_file.csv";
+            String destPath = "/home/tania/input/test_file2.csv";
             File dest = new File(destPath);
 
 
             try {
-                FileUtils.copyFile(source,dest);
+                FileUtils.copyFile(source, dest);
 
             } catch (IOException e) {
                 e.printStackTrace();
@@ -83,13 +50,13 @@ class WatchServiceTest {
         createFilethread.start();
 
 
-        try {
+        /*try {
 
             Thread.sleep(1000);
 
         } catch (InterruptedException e) {
             e.printStackTrace();
-        }
+        }*/
 
 
     }
